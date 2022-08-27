0b2efa51881ed10a96828fca5f72c451b16d76fd
Nicole Carvalho <nicolecarvalho.ti@hotmail.com>
2022-08-27 17:27:55 -0300

    Boas-vindas somente com Python

---
+++ app.py
@@ -0,0 +1,9 @@
+#Criação de método
+def welcome(name):
+    return "Olá " + name + ", bem-vindo(a) ao Curso básico de Robot Framework!"
+
+#Invocação do método dentro de uma variável
+result = welcome("Nicole")
+
+#Impressão do valor que a variável armazenou
+print(result)
